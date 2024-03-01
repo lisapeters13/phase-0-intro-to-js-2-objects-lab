12a77614e5b024111e39b7ffab6786786aac0aa3
lisapeters13 <112504985+lisapeters13@users.noreply.github.com>
2024-03-01 18:41:07 -0500

    sending work to github

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name:"lisa",
+    address:"brooklyn",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copyofemployee = {...employee}
+    copyofemployee[key] = value; //Why are we using bracket notation here?
+  
+    return copyofemployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
+  function deleteFromEmployeeByKey(employee, key){
+const copy = {...employee}
+delete copy[key]
+return copy;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+delete employee [key]
+return employee;
+  }
